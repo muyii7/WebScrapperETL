9cee086a8a98db9c239fae9659c6ea3d63ab0dc2
muyii7 <muyiwa.odusanya@gmail.com>
2024-04-29 07:41:54 +0100

    added function to transform extracted data

--- etl.py
+++ etl.py
@@ -17,7 +17,7 @@ def scrape_data():
     # A list to aggregate the matched/desired csv links  
     csv_links = []
     for link in links:
-        # A regular expression to match the ending strings of a link
+        # A regular expression to match the ending strings of a link        
         if re.search(r'mmz\d+\/\d+\/(E0|E1|E2)\.csv', str(link)):
             csv_link = re.search(r'mmz\d+\/\d+\/(E0|E1|E2)\.csv', str(link)).group()
             csv_link = 'https://www.football-data.co.uk/'+ csv_link # Append ending string to base link
@@ -38,4 +38,23 @@ def extract_data():
     combined_data = pd.concat(datafiles, axis=0, ignore_index=True) # Merge all data from each csv file into a single dataframe
     # Write data to a csv file. This file serves as a staging layer before performing transformation
     combined_data.to_csv('raw/football_data.csv', header = ['div','date','home_team','away_team','fthg','ftag'], index = False)
-extract_data()
+
+
+#Transform all the combined extracted data to suit the purpose of future analytics
+def transform_data():
+    football_data = pd.read_csv('./football_data.csv')
+    # Define a function to convert the date column to a uniform date format
+    def convert_date(value):
+        # RegEx to match the date string format - dd\mm\yyyy 03/02/2023
+        if re.search(r'\d+\/\d+\/\d\d\d\d', str(value)):
+            new_date = datetime.strptime(str(value), '%d/%m/%Y').date() # Convert date string to date type
+            return new_date
+        elif re.search(r'\d+\/\d+\/\d\d', str(value)): # RegEx to match the date string format - dd\mm\yy
+            new_date = datetime.strptime(str(value), '%d/%m/%y').date() # Convert to dd\mm\yyyy format & date type
+            return new_date
+        else:
+            pass
+    football_data['date'] = football_data['date'].apply(convert_date) # Apply convert function to date column
+    football_data.to_csv('transformed/transformed_data.csv') # Export cleaned data to an external file
+    return football_data
+transform_data()
